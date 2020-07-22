3b76532d2283c81a491b042a2656c056fd60d1a6
Alex <alexandervandoren@yahoo.com>
2020-07-22 15:06:03 +0200

    get use of strange refresh stuff but it's gonna be alright

--- src/Components/Sidebar.svelte
+++ src/Components/Sidebar.svelte
@@ -1,3 +1,29 @@
-<div>
-  Sidebar
-</div>
+<script>
+  let shown = true;
+</script>
+
+<style>
+  aside.sidebar{
+  position: fixed;
+  height: 100%;
+  background-color: greenyellow;
+  min-width: 20%;
+  
+  }
+
+  @media screen and (max-width: 700px) {
+    aside.sidebar{
+    text-align: center;
+    float: none;
+    width: 100%;
+  }
+}
+</style>
+<svelte:head>
+	<link rel="stylesheet" href="../style.css" />
+</svelte:head>
+
+
+<aside class="sidebar">
+
+</aside>

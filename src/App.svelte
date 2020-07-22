c4711d2f4f75034a4b06c564d0217c8097152970
Alex <alexandervandoren@yahoo.com>
2020-07-22 14:14:36 +0200

    create basic components
    was also working in the wrong files locally

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,11 @@
 <script>
+import Topbar from "./Components/Topbar.svelte";
+import Sidebar from "./Components/Sidebar.svelte";
+
 	let name = 'Finlay';
 </script>
 
 <h1>Hello {name}!</h1>
+
+<Topbar />
+<Sidebar />

358f1d28c884b9d29fd88ac6e26175f3eca266e0
Finlay Degrauwe <finlaydegrauwe@gmail.com>
2020-07-23 15:15:57 +0200

    Sidebar updated design

--- src/Components/Sidebar.svelte
+++ src/Components/Sidebar.svelte
@@ -1,19 +1,15 @@
 <script>
-  const side_title = "Invite people you trust into your bubble " ;
+  const side_title = "Invite people you trust into your bubble!" ;
   let shown = true;
+  import iconMail from 'heroicons-ui/icons/icon-mail.svg';
+  import iconChat from 'heroicons-ui/icons/icon-chat.svg';
+  import iconPrinter from 'heroicons-ui/icons/icon-printer.svg';
 </script>
 
 <style>
-
-
-  @media screen and (max-width: 700px) {
     aside.sidebar{
-    text-align: center;
     float: none;
     width: 100%;
-  }
-
-
 }
 </style>
 <svelte:head>
@@ -22,25 +18,49 @@
 
 
 <aside class="sidebar relative h-full min-h-screen">
-  <h1 class="text-blue-800 text-2xl p-8">{side_title}</h1>
-<div class="p-8">Select bubble
-  <select name="cars" id="cars">
-    <option value="bubble1">UX Design</option>
-    <option value="bubble2">Food</option>
-    <option value="bubble3">Sports</option>
-    <option value="bubble4">Leisure</option>
-  </select>
-  <button id="newBubble">Create new bubble</button>
-  
-  Your bubble invite link
-  <input type="text" id="bubbleLink" name="bubbleUrl"><button id="newBubble">Copy</button>
-  
-  Share via:
-<ul>
-    <li>Email</li>
-    <li>Text message</li>
-    <li>Printer</li>
-</ul>
+  <h1 class="text-blue-800 text-center text-2xl p-8">{side_title}</h1>
+  <div class="p-8">
+<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+        Select your bubble
+      </label>
+<select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+          <option>New Mexico</option>
+          <option>Missouri</option>
+          <option>Texas</option>
+        </select><p>
+<button class="mt-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+  New bubble
+</button>
+
+  </p><form class="mt-8 mb-8 w-full max-w-sm">
+    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+        Your invite link:
+      </label>
+  <div class="flex items-center border-b border-b-2 border-teal-500 py-2">
+    <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="link" aria-label="Full name">
+    <button class="flex-shrink-0 bg-grey-500 hover:bg-grey-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+      Copy link
+    </button>
+  </div>
+</form>
+  <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 mt-2" for="grid-state">
+        Share via:
+      </label>
+
+  <div class="px-2">
+  <div class="text-center text-gray-700 text-xs flex -mx-2">
+    <div class="w-1/3 px-2">
+      <div class="h-16">{iconMail} Email</div>
+    </div>
+    <div class="w-1/3 px-2">
+     
+      <div class="h-16">{iconChat} Text message</div>
+    </div>
+    <div class="w-1/3 px-2">
+      <div class="h-16">{iconPrinter} Printer</div>
+    </div>
+  </div>
+</div>
 </div>
   <!-- Invite link -->
   <!-- 

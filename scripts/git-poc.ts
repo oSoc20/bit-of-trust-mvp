fc1ae2806a6bc6c7daabe18282b6b62b45d51c77
Les De Ridder <les@lesderid.net>
2020-07-23 16:25:53 +0200

    Refactor git relationship stuff more
    Still TODO:
    * add metadata storage (names associated with tokens etc.) => git notes?

--- scripts/git-poc.ts
+++ scripts/git-poc.ts
@@ -9,6 +9,15 @@ import http from "isomorphic-git/http/node";
 type Token = Uint8Array;
 const TokenSize = 16;
 
+type RelationshipName = string;
+
+let gitOpts = {
+    fs, http,
+    corsProxy: 'https://cors.isomorphic-git.org',
+    url: 'https://lesderid.net/bit-of-trust.git',
+    dir: 'repo'
+};
+
 function createToken(): Token {
     let token = new Uint8Array(TokenSize);
     crypto.getRandomValues(token);
@@ -19,77 +28,151 @@ function tokenToString(token: Token): string {
     return Buffer.from(token).toString('hex');
 }
 
-async function addTokensToRelationship(tokens: Array<Token>, relationship: string): Promise<void> {
-    let gitOpts = {
-        fs, http,
-        corsProxy: 'https://cors.isomorphic-git.org',
-        url: 'https://lesderid.net/bit-of-trust.git',
-        dir: 'repo'
-    };
+class Relationship {
+    private changed: boolean = false;
+    private unpushedChanges: boolean = false;
 
-    await git.clone({noCheckout: true, ...gitOpts});
+    private get path(): string {
+        return gitOpts.dir + '/' + this.name;
+    }
 
-    try {
+    async getHash(): Promise<string> {
+        return await git.resolveRef({ref: this.name, ...gitOpts});
+    }
+
+    private constructor(public name: RelationshipName) {
+    }
+
+    static async get(name: RelationshipName, createIfNew: boolean = false): Promise<Relationship> {
+        await git.clone({noCheckout: true, ...gitOpts});
+
         try {
-            await git.fetch({ref: relationship, ...gitOpts});
-        } catch {
-            // branch doesn't exist on remote
+            try {
+                await git.fetch({ref: name, ...gitOpts});
+            } catch {
+                // branch doesn't exist on remote
+            }
+
+            // branch doesn't exist on remote, but we might have it locally
+            await git.checkout({ref: name, ...gitOpts});
+        } catch (e) {
+            // branch doesn't exist on remote nor locally
+
+            if (createIfNew) {
+                await git.checkout({ref: "dummy", ...gitOpts});
+                await git.branch({
+                    ref: name,
+                    checkout: true,
+                    ...gitOpts
+                });
+            } else {
+                return null;
+            }
+        }
+        return new Relationship(name);
+    }
+
+    private async isCheckedOut(): Promise<boolean> {
+        let currentBranch = await git.currentBranch({...gitOpts});
+        return currentBranch == this.name;
+    }
+
+    private async ensureCheckedOut(): Promise<void> {
+        if (!await this.isCheckedOut()) {
+            await git.checkout({ref: this.name, ...gitOpts});
+        }
+    }
+
+    private async ensureFileExists(): Promise<void> {
+        await this.ensureCheckedOut();
+
+        let files = await git.listFiles({ref: this.name, ...gitOpts});
+        if (!files.includes(this.name)) {
+            await fs.promises.writeFile(this.path, "");
+            this.changed = true;
+        }
+    }
+
+    async getTokens(): Promise<Array<Token>> {
+        await this.ensureFileExists();
+
+        let lines = (await fs.promises.readFile(this.path)).toString().split('\n');
+        return lines
+            .filter(l => l.length > 0)
+            .map(l => new Uint8Array(l
+                .match(/.{1,2}/g)
+                .map(byte => parseInt(byte, 16))));
+    }
+
+    private async includesToken(token: Token): Promise<boolean> {
+        let tokens = await this.getTokens();
+        return tokens.includes(token);
+    }
+
+    async addToken(token: Token): Promise<boolean> {
+        if (await this.includesToken(token)) return false;
+
+        await fs.promises.appendFile(this.path, tokenToString(token) + '\n');
+        this.changed = true;
+
+        return true;
+    }
+
+    async commitChanges(): Promise<boolean> {
+        if (!this.changed) {
+            return false;
         }
 
-        // branch doesn't exist on remote, but we might have it locally
-        await git.checkout({ref: relationship, ...gitOpts});
-    } catch (e) {
-        // branch doesn't exist on remote nor locally
+        await git.remove({filepath: ".", ...gitOpts});
+        await git.add({filepath: this.name, ...gitOpts});
 
-        await git.checkout({ref: "dummy", ...gitOpts});
-        await git.branch({
-            ref: relationship,
-            checkout: true,
+        let hash = await git.commit({
+            author: {
+                name: 'Bit of Trust System',
+                email: 'system@bit-of-trust',
+            },
+            message: "Add member(s) to relationship",
             ...gitOpts
         });
+
+        this.unpushedChanges = true;
+
+        return true;
     }
 
-    let files = await git.listFiles({ref: relationship, ...gitOpts});
-    let path = gitOpts.dir + '/' + relationship;
-    let fileChanged = false;
-    if (!files.includes(relationship)) {
-        await fs.promises.writeFile(path, "");
-        fileChanged = true;
+    async push(): Promise<boolean> {
+        //TODO: don't return false?
+        if (!this.unpushedChanges) return false;
+
+        let pushResult = await git.push({...gitOpts});
+
+        //TODO: check if this is correct
+        return pushResult.ok != undefined;
     }
-
-    let lines = (await fs.promises.readFile(path)).toString().split('\n');
-    for (let token of tokens) {
-        if (!lines.includes(tokenToString(token))) {
-            await fs.promises.appendFile(path, tokenToString(token) + '\n');
-            fileChanged = true;
-        }
-    }
-
-    if (!fileChanged) {
-        //TODO: return something useful
-        return;
-    }
-
-    await git.remove({filepath: ".", ...gitOpts});
-    await git.add({filepath: relationship, ...gitOpts});
-
-    let hash = await git.commit({
-        author: {
-            name: 'Bit of Trust System',
-            email: 'system@bit-of-trust',
-        },
-        message: "Add member(s) to relationship",
-        ...gitOpts
-    });
-
-    let pushResult = await git.push({...gitOpts});
-    //TODO: check if push was successful
 }
 
-let tokens = [createToken(), createToken()];
-let relationship = "my-relationship";
+async function main() {
+    let token = createToken();
 
-addTokensToRelationship(tokens, relationship)
+    let relationships = await Promise.all(
+        [
+            "my-relationship",
+            "my-second-relationship",
+            "my-third-relationship"
+        ].map(n => Relationship.get(n)));
+
+    for (let relationship of relationships.filter(r => r != null)) {
+        await relationship.addToken(token);
+        await relationship.commitChanges();
+        await relationship.push();
+
+        let hash = await relationship.getHash();
+        console.log(`'${relationship.name}' hash: ${hash}`);
+    }
+}
+
+
+main()
     .then(() => {
         console.log("main() ended");
     })

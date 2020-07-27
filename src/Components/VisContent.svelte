7c93917fb771066e0a64369fe9a3fef60298bc2b
Les De Ridder <les@lesderid.net>
2020-07-27 20:45:32 +0200

    Improve visualisation

--- src/Components/VisContent.svelte
+++ src/Components/VisContent.svelte
@@ -1,5 +1,8 @@
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css">
+<i class="far fa-user" style="visibility:hidden;"></i>
+<i class="fal fa-user" style="visibility:hidden;"></i>
+
 <script lang="ts">
-  import {onMount} from 'svelte';
   import {DataSet, Network} from "vis-network/standalone";
   import Relationship from "../git/relationship";
   import {tokenToString} from "../git/token";
@@ -10,16 +13,74 @@
     let relationships = await Relationship.getAll();
 
     const data = {nodes: new DataSet(), edges: new DataSet()};
-    const options = {};
+    const options = {
+      nodes: {
+        borderWidth: 2,
+        color: "#2c5282",
+        font: {
+          color: "#2c5282",
+          face: "Roboto",
+          size: 16
+        }
+      },
+      edges: {
+        width: 2,
+        color: "#2c5282",
+      },
+      layout: {
+        hierarchical: {
+          enabled: false
+        },
+      },
+      physics: {
+        enabled: true,
+        solver: 'barnesHut',
+        barnesHut: {
+          avoidOverlap: 0.5
+        }
+      },
+      groups: {
+        knownUsers: {
+          shape: 'icon',
+          icon: {
+            face: '"Font Awesome 5 Free"',
+            color: "#2c5282",
+            code: '\uf007',
+            weight: 700,
+            size: 24,
+          }
+        },
+        unknownUsers: {
+          shape: 'icon',
+          icon: {
+            face: '"Font Awesome 5 Free"',
+            color: "#2c5282",
+            code: '\uf007',
+            weight: 300,
+            size: 24
+          },
+        },
+        relationships: {
+          shape: 'icon',
+          icon: {
+            face: '"Font Awesome 5 Free"',
+            color: "#2c5282",
+            code: '\uf0c0',
+            weight: 700,
+            size: 48
+          },
+        }
+      }
+    };
 
     for (let relationship: Relationship of relationships) {
       let hash = await relationship.getHash();
-      data.nodes.add({id: hash, label: relationship.name});
+      data.nodes.add({id: hash, label: relationship.name, group: "relationships"});
 
       for (let token of await relationship.getTokens()) {
         let tokenString = tokenToString(token);
         if (!data.nodes.getIds().some(id => id === tokenString)) {
-          data.nodes.add({id: tokenString, label: tokenString});
+          data.nodes.add({id: tokenString, label: "", value: 1, group: "unknownUsers"});
         }
         data.edges.add({from: tokenString, to: hash});
       }
@@ -30,10 +91,17 @@
   })();
 </script>
 
+<style>
+  #full-network {
+    width: 100%;
+    height: 90%;
+  }
+</style>
+
 <div id="full-network" bind:this={networkContainer} style="display:none"></div>
 
 {#await promise}
-  <p>Getting network</p>
+  <p>Getting network...</p>
 {:catch error}
   <p>Error getting network: {console.error("%O", error), error}</p>
 {/await}

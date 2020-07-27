0b6cda9320cb8cfc9876587feea972e0740e915e
Les De Ridder <les@lesderid.net>
2020-07-27 16:30:12 +0200

    Add basic visualition

--- src/Components/Content.svelte
+++ src/Components/Content.svelte
@@ -1,6 +1,6 @@
 <div class="bg-blue-200 h-full shadow-inner">
   <div class="text-center w-full text-grey-darkest">
-    
+
     <slot></slot>
   </div>
 </div>
--- src/Components/VisContent.svelte
+++ src/Components/VisContent.svelte
@@ -1,6 +1,39 @@
-<script>
+<script lang="ts">
+  import {onMount} from 'svelte';
+  import {DataSet, Network} from "vis-network/standalone";
+  import Relationship from "../git/relationship";
+  import {tokenToString} from "../git/token";
 
-  import getAllBubbles from "../Data/GetData";
-  console.info("All B", getAllBubbles());
+  let networkContainer;
+
+  let promise = (async () => {
+    let relationships = await Relationship.getAll();
+
+    const data = {nodes: new DataSet(), edges: new DataSet()};
+    const options = {};
+
+    for (let relationship: Relationship of relationships) {
+      let hash = await relationship.getHash();
+      data.nodes.add({id: hash, label: relationship.name});
+
+      for (let token of await relationship.getTokens()) {
+        let tokenString = tokenToString(token);
+        if (!data.nodes.getIds().some(id => id === tokenString)) {
+          data.nodes.add({id: tokenString, label: tokenString});
+        }
+        data.edges.add({from: tokenString, to: hash});
+      }
+    }
+
+    networkContainer.style = "";
+    const network = new Network(networkContainer, data, options);
+  })();
 </script>
 
+<div id="full-network" bind:this={networkContainer} style="display:none"></div>
+
+{#await promise}
+  <p>Getting network</p>
+{:catch error}
+  <p>Error getting network: {console.error("%O", error), error}</p>
+{/await}

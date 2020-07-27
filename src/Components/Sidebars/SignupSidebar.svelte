834e75269ca63ef20e18df778e47dc85adbe17ff
Finlay Degrauwe <finlaydegrauwe@gmail.com>
2020-07-27 13:20:48 +0200

    Met afbeelding

--- src/Components/Sidebars/SignupSidebar.svelte
+++ src/Components/Sidebars/SignupSidebar.svelte
@@ -1,13 +1,11 @@
 <script>
   import { Upload, ArrowNarrowRight } from 'svelte-hero-icons';
   import BubbleList from '../BubbleList.svelte';
-import BackToList from '../Buttons/BackToList.svelte';
   const side_title = 'Invite people you trust into your bubble ';
   let shown = true;
 </script>
 
 <aside class="sidebar relative h-full min-h-screen p-8">
-  <BackToList />
  <form class="mt-24 mb-8 w-full max-w-sm">
       <label
         class=" uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -33,7 +31,30 @@ import BackToList from '../Buttons/BackToList.svelte';
         Alias picture
       </label>
 
-
+<section class="text-gray-700 body-font pt-2">
+  <div class="container mx-auto flex py-2 items-center">
+    <div>
+      <img class="object-cover object-center rounded-full" alt="hero" src="https://dummyimage.com/600x600">
+    </div>
+    <div class=" md:w-1/2 pl-4 flex flex-col items-start text-left items-center text-center">
+      <div class="justify-center">
+       <button class="bg-gray-300 ml-4 mb-4 hover:bg-gray-400 text-xs text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+        <Upload size="24" />
+        <span class="pl-2">Select from library</span>
+       </button>
+        <button class="bg-gray-300 ml-4 hover:bg-gray-400 text-xs text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+        <Upload size="24" />
+        <span class="pl-2">Upload from computer</span>
+       </button>      </div>
+    </div>
+  </div>
+</section>
+      <div class="text-right mt-16">
+       <button class="bg-teal-300 hover:bg-teal-400 text-xs text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+        <span class="pr-6 pl-6">Set up</span>
+        <ArrowNarrowRight size="24" />
+       </button>
+       </div>
 </aside>
 
 <style>

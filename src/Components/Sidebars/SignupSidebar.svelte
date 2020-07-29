7a89e2d588e01d44cb49775227e3eeab38a27780
Alex <alexandervandoren@yahoo.com>
2020-07-29 14:32:48 +0200

    Generate invite url frontend

--- src/Components/Sidebars/SignupSidebar.svelte
+++ src/Components/Sidebars/SignupSidebar.svelte
@@ -9,11 +9,20 @@ import { goto, url } from '@sveltech/routify';
   function signUpuser() {
 
     if(name){
-      $goto("/BubbleOverview");
+      let id = guidGenerator();
+ 
       user.login(name);
-      
+      localStorage.setItem("token", id);
+      $goto("/BubbleOverview");
     }
   }
+
+  function guidGenerator() {
+    var S4 = function() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
 </script>
 
 <aside class="sidebar relative h-full min-h-screen p-8">
